7089fb5597a6614e94ecf51c1ff318891c707e84
Joyce <afunwachidimma07@gmail.com>
2025-12-04 02:10:13 -0800

    Add files via upload

--- script/index2.js
+++ script/index2.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const email = localStorage.getItem("confEmail") || "guest@example.com";
     const user = localStorage.getItem("confUser") || "@unknown";
     const avatar = localStorage.getItem("confAvatar");
-    const ticketId = localStorage.getItem("confTicketId") || "#00000";
+    const ticketId = localStorage.getItem("confTicketId") || "";
 
 
     document.getElementById("h1").innerHTML = `Congrats, <span class="spa">${name}</span> ! your ticket is ready.`;
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     document.getElementById("name").innerText = name;
     document.getElementById("email").innerText = user;
-    document.getElementById("p4").innerText = ticketId;
+    // document.getElementById("p4").innerText = ticketId;
 
     const avatarEl = document.getElementById("avatar");
     if (avatar) {
